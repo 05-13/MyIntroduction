55e6612297fe8fb6943f8920fd5b374d5325bdd0
CzhCN1 <2021525982@qq.com>
2016-08-25 16:36:28 +0800

    add page project1 && change the readme

--- js/H5.js
+++ js/H5.js
@@ -86,6 +86,59 @@ var H5 = function(){
 			$.fn.fullpage.moveTo(firstPage);
 		}
 	};
+	//是否是第一次调用该函数
+	//如果是第一次，执行H5_loading函数
 	this.loader = (typeof H5_loading == 'function') ? H5_loading : this.loader;
 	return this;
+};
+
+
+/**
+ * [H5_loading description]
+ * @param {[type]} images    [description]
+ * @param {[type]} firstPage [description]
+ */
+var H5_loading = function(images,firstPage){
+	//读取图片资源
+	var id = this.id;
+	if(this._images === undefined){	//第一次进入
+		this._images = (images || []).length;	//需要加载图片数
+		this._loaded = 0;						//已加载图片数
+
+		window[id] = this;
+
+		for(var i = 0;i<images.length;i++){
+			var img = new Image();
+			img.onload = function(){
+				window[id].loader();
+			};
+			img.src = images[i];
+		}
+		$('.loadingRate').text('0%');
+		return this;
+	}else{
+		this._loaded++;
+		$('.loadingRate').text( ( (this._loaded / this._images * 100) >> 0) + '%');
+		if(this._loaded < this._images){
+			return this;
+		}
+	}
+	window[id] = null;
+
+	//fullpage样式
+	this.ele.fullpage({
+		onLeave : function(index,nextIndex,direction){
+			//上一页离开事件
+			$(this).find('.component').trigger('onLeave');
+		},
+		afterLoad : function(anchorLink,index){
+			//当前页载入事件
+			$(this).find('.component').trigger('afterLoad');
+		}
+	});
+	this.ele.show();		//页面显示
+
+	if(firstPage){
+		$.fn.fullpage.moveTo(firstPage);
+	}
 };
